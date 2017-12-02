f46281d4f11b3efc5de63392fa73a6b7d3956bde
adfaure <adrien.faure2@gmail.com>
2017-12-02 17:00:08 +0100

    add bullets and range

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -6,10 +6,19 @@ extends RigidBody2D
 
 var direction = Vector2(1, 0)
 var velocity = 200
+var dist_range = 40
 
 func _ready():
-	set_fixed_process(false)
+	print("bullet")
+	set_fixed_process(true)
 
-func _fixed_process(delta):
-	self.move(direction.normalized() * velocity * delta)
+func init_bullet(velocity):
+	self.set_linear_velocity(velocity)
 	
+func _fixed_process(delta):
+	var motion = direction.normalized() * velocity * delta
+	print(self.dist_range)
+	self.dist_range -= direction.length()
+	
+	if self.dist_range <= 0:
+		self.queue_free()
