81c13d9d0cff895e199dcfbe675a9c92200a01fc
Avzgui <contact.avzgui@gmail.com>
2017-12-03 15:13:38 +0100

    move camera to the left

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -12,3 +12,13 @@ func _ready():
 func _fixed_process(delta):
 	if self.player.dead and !self.player.get_node("AnimationPlayer").is_playing():
 		get_tree().set_pause(true)
+	
+	# Move camera
+	var camera_pos = get_node("Camera2D").get_camera_pos()
+	var visible_rect = get_node("Camera2D").get_viewport().get_visible_rect()
+	
+	# To the left
+	if player.get_pos().x < camera_pos.x :
+		var slide = Vector2(-visible_rect.size.width, 0)
+		var new_pos = camera_pos + slide 
+		get_node("Camera2D").set_pos(new_pos)
