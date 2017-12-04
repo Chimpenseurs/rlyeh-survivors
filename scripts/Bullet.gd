f0cb54ce6d7bf718db34b2327efece77ae0f47b4
Meinew <moi.meinew@gmail.com>
2017-12-04 21:58:02 +0100

    Fixed bullet in wall

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -5,6 +5,7 @@ export var dist_range = 100
 var motion = Vector2(0, 0)
 var damages = 0
 var pierce = 1
+var age= 0
 
 func _ready():
 	set_fixed_process(true)
@@ -18,6 +19,7 @@ func init_bullet(velocity, damages, dist_range):
 	self.set_linear_velocity(self.motion)
 
 func _fixed_process(delta):
+	age += delta
 	if self.dist_range <= 0:
 		self.queue_free()
 	self.dist_range -= motion.length()
@@ -29,5 +31,6 @@ func _on_Bullet_body_enter( body ):
 		pierce -= 1
 		if pierce == 0:
 			self.queue_free()
-	elif !body.is_in_group("player") and !body.is_in_group("bullet"):
+			# TODO : fix bullet with better solution than age
+	elif !body.is_in_group("player") and !body.is_in_group("bullet") and age >0.1:
 		self.queue_free()
