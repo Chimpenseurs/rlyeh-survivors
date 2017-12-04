8bc019be74b9e68af1e3d6e6d210027bc11f0b2b
adfaure <adrien.faure2@gmail.com>
2017-12-04 21:20:20 +0100

    Max enemies (setted

---
+++ scripts/EnemiesHolder.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+export var max_enemies = 2
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func add_enemy(node):
+	if self.get_child_count() < self.max_enemies:
+		self.add_child(node)
+	else:
+		print("ezrezr")
+		node.queue_free()
+		
+func is_max_reached():
+	return self.get_child_count() >= self.max_enemies
