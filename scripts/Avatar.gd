d43d6c0006357425e556803a39d2bdab6d70d7c8
Avzgui <contact.avzgui@gmail.com>
2017-12-04 09:19:22 +0100

    add weapon list

--- scripts/Avatar.gd
+++ scripts/Avatar.gd
@@ -2,16 +2,28 @@ extends KinematicBody2D
 
 var bulletTscn = preload("res://scenes/Bullet.tscn")
 
+var weapons = {
+	"pistol":{
+		"fire_rate": 0.75,
+		"damage": 50
+	},
+	"machine gun":{
+		"fire_rate": 0.2,
+		"damage": 50
+	}
+}
+
+var enabled_weapon = ["pistol"]
+var current_weapon = weapons["pistol"]
+
 const shoot_speed = 500
-const fire_rate = 0.2 # lets say in ms
 const velocity = 350
 
 var bullet_range = 50000
-var damage = 50
 var fire_ready = 0
 var dead = false
 
-const max_life = 5000
+var max_life = 5000
 var life = 5000
 
 var devil_hearts = 0
@@ -52,14 +64,14 @@ func _fixed_process(delta):
 
 func _shoot_arrow(delta):
 	if self.fire_ready == 0:
-		self.fire_ready = fire_rate
+		self.fire_ready = self.current_weapon["fire_rate"]
 		var bullet_motion = (get_global_mouse_pos() - self.get_global_pos()).normalized() * shoot_speed
 		var new_arrow = bulletTscn.instance()
 		var arrow_rotation = get_angle_to(get_global_mouse_pos()) + self.get_rot()
 		new_arrow.set_rot(arrow_rotation)
 		new_arrow.set_global_pos(self.get_global_pos())
 		get_parent().bullerHolder.add_child(new_arrow)
-		new_arrow.init_bullet(bullet_motion, self.damage, self.bullet_range )
+		new_arrow.init_bullet(bullet_motion, self.current_weapon["damage"], self.bullet_range )
 
 func set_player_orientation():
 	var self_pos = self.get_pos()
